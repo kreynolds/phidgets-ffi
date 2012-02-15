56ff200e9972b8042d3092d4a238f7ce2898e41b
mphi <mphi@phidgets.com>
2012-02-15 12:43:51 -0700

    Fixed OpenSuse 64 bit bug with libphidget21.so

--- lib/phidgets-ffi/phidgets-ffi.rb
+++ lib/phidgets-ffi/phidgets-ffi.rb
@@ -6,7 +6,17 @@ module Phidgets
   	if Config::CONFIG['target_os'] =~ /darwin/ #Mac OS X
       ffi_lib '/Library/Frameworks/Phidget21.framework/Versions/Current/Phidget21'
     else # Linux
-      ffi_lib '/usr/lib/libphidget21.so' 
+  
+      if File.exists?('/usr/lib64/libphidget21.so') 
+        ffi_lib '/usr/lib64/libphidget21.so' 
+      elsif File.exists?('/usr/lib32/libphidget21.so') 
+        ffi_lib '/usr/lib32/libphidget21.so' 
+      elsif File.exists?('/usr/lib/libphidget21.so')
+        ffi_lib '/usr/lib/libphidget21.so' 
+      else
+        puts 'Error: Cannot find libphidget21.so. Please install the Phidget Drivers'
+      end    
+    
     end
 
   end
