fc6349d5dd7df09fbd09904deab0075cfb3c150d
mphi <mphi@phidgets.com>
2012-02-16 10:11:37 -0700

    Used a better way to handle the finding of the Phidget drivers.

--- lib/phidgets-ffi/phidgets-ffi.rb
+++ lib/phidgets-ffi/phidgets-ffi.rb
@@ -3,27 +3,7 @@ module Phidgets
   
     extend ::FFI::Library
 
-  	if Config::CONFIG['target_os'] =~ /darwin/ #Mac OS X
-      ffi_lib '/Library/Frameworks/Phidget21.framework/Versions/Current/Phidget21'
-    else # Linux
-  
-      if File.exists?('/usr/lib64/libphidget21.so') 
-        ffi_lib '/usr/lib64/libphidget21.so' 
-      elsif File.exists?('/usr/lib32/libphidget21.so') 
-        ffi_lib '/usr/lib32/libphidget21.so' 
-      elsif File.exists?('/usr/lib/libphidget21.so')
-        ffi_lib '/usr/lib/libphidget21.so' 
-      else
-        puts 'Error: Cannot find libphidget21.so. Please install the Phidget Drivers'
-      end    
-    
-    end
+    ffi_lib ['/Library/Frameworks/Phidget21.framework/Versions/Current/Phidget21', '/usr/lib/libphidget21.so', '/usr/lib64/libphidget21.so', '/usr/lib32/libphidget21.so', 'Phidget21', 'libphidget21.so']
 
   end
 end
-
-
-
-
-
-
